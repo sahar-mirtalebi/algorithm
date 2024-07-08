e9de7df305a81875d39386b9b4f3733f69f83e0b
sahar <sahar.mirtalebi@gmail.com>
2024-07-08 15:11:06 +0330

    Updated fibonacci.go

--- algorithm_fibonacci/fibonacci.go
+++ algorithm_fibonacci/fibonacci.go
@@ -6,13 +6,20 @@ func main() {
 
 	var numA = 0
 	var numB = 1
-	var numList = []int{}
+	var numList = []int{numA, numB}
+	var inputNum int
+	fmt.Scan(&inputNum)
 
 	for num := 0; num < 18; num++ {
 		var nuwNum = numA + numB
-		numList = append(numList, nuwNum)
-		numA = numB
-		numB = nuwNum
+		if nuwNum <= inputNum {
+			numList = append(numList, nuwNum)
+			numA = numB
+			numB = nuwNum
+		} else {
+			break
+		}
 	}
-	fmt.Println(numList)
+	fmt.Printf("the resault of %v:\n", inputNum)
+	fmt.Printf("series %v\n", numList)
 }
