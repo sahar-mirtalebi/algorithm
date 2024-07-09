6951cf8569adb7027be68088c15d5b7e755d0a24
sahar <sahar.mirtalebi@gmail.com>
2024-07-09 15:58:09 +0330

    updated fibonacci algorithm add function

--- algorithm_fibonacci/fibonacci.go
+++ algorithm_fibonacci/fibonacci.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
+var numA = 0
+var numB = 1
+
 func main() {
-
-	var numA = 0
-	var numB = 1
-	var numList = []int{numA, numB}
 	var inputNum int
-	fmt.Scan(&inputNum)
+	fibonacci(inputNum)
+}
 
-	for num := 0; num < 18; num++ {
+func fibonacci(inputNum int) {
+	fmt.Scan(&inputNum)
+	var numList = []int{numA, numB}
+	for num := 0; num < inputNum; num++ {
 		var nuwNum = numA + numB
 		if nuwNum <= inputNum {
 			numList = append(numList, nuwNum)
